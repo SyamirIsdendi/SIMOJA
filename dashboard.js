e1708c6b152c48700be86a53faa2e54373921f03
SyamirIsdendi <syamirisdendi@gmail.com>
2025-06-02 10:22:27 +0700

    Update dashboard.js
    Signed-off-by: SyamirIsdendi <syamirisdendi@gmail.com>

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,21 @@ const relayRef = db.ref("relayStatus"); // ← ini diperbaiki ya!
 
 tempRef.on('value', (snapshot) => {
   document.getElementById("temp").textContent = snapshot.val();
+<<<<<<< HEAD
+});
+
+humRef.on('value', (snapshot) => {
+  document.getElementById("humidity").textContent = snapshot.val();
+});
+
+arusRef.on('value', (snapshot) => {
+  document.getElementById("current").textContent = snapshot.val();
+});
+
+relayRef.on('value', (snapshot) => {
+  document.getElementById("relay").textContent = snapshot.val();
+=======
+>>>>>>> 10949df5b296dc37b91c43b20417db2484bb34aa
 });
 
 humRef.on('value', (snapshot) => {
